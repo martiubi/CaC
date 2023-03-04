96a3909a5a18cf6c0492d96840a9d9bc28e0de9e
martiubi <martinaubilla295@gmail.com>
2023-03-04 13:16:13 -0300

    -

--- js/script.js
+++ js/script.js
@@ -65,8 +65,7 @@ cancelarBtn.addEventListener('click', (e) => {
 
 comprarBtn.addEventListener('click', (e) => {
   resumenMessage[0].style.display = 'none';
-  exitoMessage[0];
-
+  exitoMessage[0].style.display = 'table';
   setTimeout(() => {
     exitoMessage[0].style.display = 'none';
   }, 2000);
